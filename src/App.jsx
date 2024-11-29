61415e0f8541869ca2e4d0e6c87f576f5c6ce6d2
Andrea Di Marco <andrea.dimarco@me.com>
2024-11-28 22:20:49 -0500

    fix: update HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import MyResponsiveRadar from "./components/charts/radar.jsx";
 import MyResponsiveWaffle from "./components/charts/waffle.jsx";
 import MyResponsiveHeatMapAfrica from "./components/charts/heatmap-africa.jsx";
@@ -23,75 +21,49 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1> */}
+    <div className="wrapper">
+      <h1>My Project</h1>
       <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        {/* <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-        <div
-          style={{ height: "300px", width: "800px", backgroundColor: "white" }}
-        >
+        <div className="chart waffle">
           <MyResponsiveWaffle data={waffleData} />
         </div>
-
-        <div
-          style={{ height: "500px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart radar">
           <MyResponsiveRadar data={radarData} />
         </div>
-
-        <div
-          style={{ height: "1200px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart heatmap-africa">
           <MyResponsiveHeatMapAfrica data={heatMapDataAfrica} />
         </div>
-
-        <div
-          style={{ height: "900px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart heatmap-americas">
           <MyResponsiveHeatMapAmericas data={heatMapDataAmericas} />
         </div>
-
-        <div
-          style={{ height: "1100px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart heatmap-europe">
           <MyResponsiveHeatMapEurope data={heatMapDataEurope} />
         </div>
-
-        <div
-          style={{ height: "500px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart heatmap-middleeast">
           <MyResponsiveHeatMapMiddleEast data={heatMapDataMiddleEast} />
         </div>
-
-        <div
-          style={{ height: "1000px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart heatmap-asiapacific">
           <MyResponsiveHeatMapAsiaPacific data={heatMapDataAsiaPacific} />
         </div>
-
-        <div
-          style={{ height: "500px", width: "800px", backgroundColor: "white" }}
-        >
+      </div>
+      <div className="card">
+        <div className="chart swarmplot">
           <MyResponsiveSwarmPlot data={swarmPlotData} />
         </div>
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+    </div>
   );
 }
 
