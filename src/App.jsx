f237903490afeacb7533c9608cb60f0acc138dc3
Phuong Diep <diepmphuong@gmail.com>
2024-11-30 11:46:49 -0500

    add: implementation status

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
             </div>
             <div className="databox">
               <span className="category highlight">
-                How many countries are actively committing to prevention
+                How many countries have started implementing prevention
                 measures?
               </span>
               <span className="count highlight">189</span>
@@ -156,36 +156,47 @@ function App() {
             </p>
 
             <div className="card-summary top-countries">
-              <div className="databox border-top">
+              <div className="databox">
+                <span className="category label">Country</span>
+                <span className="count label">Status</span>
+              </div>
+              <div className="databox">
+                <span className="category">Albania</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Argentina</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Croatia</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Cyprus</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Lithuania</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Mauritania</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">New Zealand</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
+                <span className="category">Republic of Moldova</span>
+                <span className="status">Full Implementation</span>
+              </div>
+              <div className="databox">
                 <span className="category highlight">
                   How many countries have committed to all key indicators?
                 </span>
                 <span className="count highlight">8</span>
-              </div>
-
-              <div className="databox">
-                <span className="category">Albania</span>
-              </div>
-              <div className="databox">
-                <span className="category">Argentina</span>
-              </div>
-              <div className="databox">
-                <span className="category">Croatia</span>
-              </div>
-              <div className="databox">
-                <span className="category">Cyprus</span>
-              </div>
-              <div className="databox">
-                <span className="category">Lithuania</span>
-              </div>
-              <div className="databox">
-                <span className="category">Mauritania</span>
-              </div>
-              <div className="databox">
-                <span className="category">New Zealand</span>
-              </div>
-              <div className="databox">
-                <span className="category">Republic of Moldova</span>
               </div>
               <span className="footnote">by alphabetical order</span>
             </div>
